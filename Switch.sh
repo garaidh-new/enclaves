937c34e0731fe36ef1a3965dadebb1e137155a43
Gary <garaidh@garaidh.net>
2021-07-16 15:06:46 +0000

    Added script to switch between them

--- Switch.sh
+++ Switch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export AWS_DEFAULT_REGION=eu-west-2
-
+export pod=""
 if [ $1 == "enclave" ]
 then 
   echo Deleting testserver
@@ -10,10 +10,18 @@ then
   echo deploying testserver into enclave
   kubectl -n foo apply -f testserver-deployment.yaml
   kubectl -n foo apply -f load-balancer.yaml
-  export pod=$(kubectl -n foo get pods | grep -m1 testserver | awk '{print $1}')
-  if [ -n "$(kubectl -n foo exec -it $pod -- ls | grep opt)" ]
+  sleep 5
+  while [ -z $pod ]
+  do 
+    export pod=$(kubectl -n foo get pods | grep Running | grep -m1 testserver | awk '{print $1}')
+    sleep 5  
+  done
+  echo $pod
+  if [ -n "$(kubectl -n foo exec -it $pod -- ls | grep -v testserver.py)" ]
   then 
-    echo 'Pod running in enclave'
+    echo 'SUCCESS. Pod running in enclave'
+  else
+    echo "Is it in an enclave?"
   fi
 fi
 
@@ -25,9 +33,17 @@ then
   echo deploying testserver without enclave
   kubectl -n foo apply -f testserver-noenclave-deployment.yaml
   kubectl -n foo apply -f load-balancer.yaml
-  export pod=$(kubectl -n foo get pods | grep -m1 testserver | awk '{print $1}')
-  if [ -n "$(kubectl -n foo exec -it $pod -- ls | grep opt)" ]
+  sleep 5
+  while [ -z $pod ]
+  do 
+    export pod=$(kubectl -n foo get pods | grep Running | grep -m1 testserver | awk '{print $1}')
+    sleep 5  
+  done
+  echo $pod
+  if [ -n "$(kubectl -n foo exec -it $pod -- ls | grep -m1 testserver.py)" ]
   then 
-    echo 'Pod running in enclave'
+    echo 'SUCCESS. Pod NOT running in enclave'
+  else
+    echo "Couldn't find the python file"
   fi
 fi
